18314cc1f373c53baa3e3e94a7315f8744e36256
Nikos Fertakis <nikos@skroutz.gr>
2017-06-09 19:34:39 +0300

    emacs: make default fonts smaller

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -984,8 +984,8 @@ comint-replace-by-expanded-history-before-point."
     ;; (set-default-font "Fantasque Sans Mono")
     ;; (set-default-font "Fira Mono")
     ;; (set-default-font "Source Code Pro")
-    (set-face-attribute 'default nil :height 140 :weight 'normal)
-    (set-face-attribute 'fixed-pitch nil :height 140 :weight 'normal)
+    (set-face-attribute 'default nil :height 120 :weight 'normal)
+    (set-face-attribute 'fixed-pitch nil :height 120 :weight 'normal)
 
     ;; Anti-aliasing
     (setq mac-allow-anti-aliasing t)))
